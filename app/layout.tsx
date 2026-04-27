40443ba2b8d45d0b9490eabae6017d9cc6eec945
euvih <vitoriakellypdasilva@gmail.com>
2026-04-27 14:46:41 -0300

    atualização

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,3 +15,23 @@ export default function RootLayout({
     </html>
   );
 }
+export const metadata = {
+  title: "Ministério Lúminuss",
+  description: "Um ministério musical que leva adoração, mensagem e esperança por meio da música.",
+  
+  openGraph: {
+    title: "Ministério Lúminuss",
+    description: "Conheça o Lúminuss e nossa missão através da música.",
+    url: "https://seusite.com",
+    siteName: "Lúminuss",
+    images: [
+      {
+        url: "https://seusite.com/logo.jpeg",
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "pt_BR",
+    type: "website",
+  },
+};
